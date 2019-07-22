c875907fb4cdd54da2023123e46d729c1ca6af5f
Shyam <bshyam@visteon.com>
2019-07-22 09:09:00 +0530

    create react template project

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory-compoent'
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory></Directory>
+    </div>
+)
+
+export default HomePage;
