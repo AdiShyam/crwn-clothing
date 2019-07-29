ac20245f4afb098c99cf02bc216f89e9ee13c3c8
Shyam <bshyam@visteon.com>
2019-07-29 12:11:19 +0530

    adidng cart icon and cart dropdow to application and implemented cart reducer

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CustomeButton from '../custom-button/custom-button.component';
+import './cart-dropdown.styles.scss';
+
+
+const CartDropDown = (  ) =>(
+    <div className="cart-dropdown">
+        <div className="cart-item" />
+        <CustomeButton>GO TO CHECKOUT</CustomeButton>
+    </div>
+)
+
+export default CartDropDown;
--- src/components/header/header.components.jsx
+++ src/components/header/header.components.jsx
@@ -6,8 +6,11 @@ import './header.styles.scss';
 
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component'
 
-const Header = ({ currentUser }) => (
+
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -21,12 +24,21 @@ const Header = ({ currentUser }) => (
                 :
                 <Link to="/signin">SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? 
+                null
+            :
+                <CartDropDown /> 
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden} })  => ({
+    currentUser,
+    hidden
+
 });
 
 export default connect(mapStateToProps)(Header);
