881e87d0eda38c367f1a46896d6df46bf8c7dc00
Shyam <bshyam@visteon.com>
2019-07-31 10:20:41 +0530

    added addItem and removeitem functionality to checkout Page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './checkout.styles.scss'
-import { connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector';
-import CheckoutItem  from '../../components/checkout-item/checkout-item.component';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
-const CheckoutPage = ({cartItems, total}) => (
-    
-    <div className= 'checkout-page'>
+const CheckoutPage = ({ cartItems, total }) => (
+
+    <div className='checkout-page'>
         <div className="checkout-header">
             <div className="header-block">
                 <span>Product</span>
@@ -27,17 +27,17 @@ const CheckoutPage = ({cartItems, total}) => (
             </div>
         </div>
         {
-            cartItems.map(cartItem  => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
+            cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
         }
         <div className='total'>
-            <span>Total: {total}</span>
+            <span>Total: ${total}</span>
         </div>
     </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-  total: selectCartTotal
+    cartItems: selectCartItems,
+    total: selectCartTotal
 })
 
 export default connect(mapStateToProps)(CheckoutPage);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -3,12 +3,30 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         cartItem => cartItem.id === cartItemToAdd.id
     );
 
-    if(exsistingCartItem) {
-        return cartItems.map(cartItem => 
+    if (exsistingCartItem) {
+        return cartItems.map(cartItem =>
             (cartItem.id === cartItemToAdd.id)
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
         )
     }
-    return [...cartItems, {...cartItemToAdd, quantity:1}]
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
+
+export const clearItmesFromCart = (cartItems, payload) => {
+    return cartItems.filter(cartItem => cartItem.id !== payload.id)
+}
+
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const exsistingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id)
+
+    if (exsistingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+    return cartItems.map(cartItem =>  (cartItem.id === cartItemToRemove.id) ?
+            {...cartItem, quantity: cartItem.quantity - 1 }
+            :
+            cartItem
+        ) 
 }
